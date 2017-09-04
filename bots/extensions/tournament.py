9ddb644861324b5466e7aeb80b5fd691c6edb344
MrTyton <joshua.gang@gmail.com>
2017-09-03 22:48:36 -0400

    Tournament extension, for mtg tournaments. Tracks players throughout the day. Works for mtg mothership, still need to figure out scg regex patterns.

---
+++ bots/extensions/tournament.py
@@ -0,0 +1,195 @@
+from Bot import ChatBot, Bot
+from discord import Embed, Color
+from requests import get
+from bs4 import BeautifulSoup as BS
+
+from datetime import date
+from pickle import load, dump
+
+import inflect
+
+from asyncio import ensure_future, sleep, Lock
+
+from pathlib import Path
+from os.path import isfile, join
+
+class TOURNAMENTBOT:
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.bot.add_player = self.add_player
+        self.bot.add_tournament = self.add_tournament
+        self.ordinalize = inflect.engine().ordinal
+        self.tournament_lock = Lock()
+        ensure_future(self.check())
+        
+
+
+    async def load_tournaments(self):
+        """
+        Loads tournaments from data folder
+        """
+        
+        if isfile(Path(self.bot.DATA_FOLDER, f'{self.bot.name}.tournaments')):
+            with open(Path(self.bot.DATA_FOLDER, f'{self.bot.name}.tournaments'), 'rb')  as fp:
+                tournament = load(fp)
+                if not tournament:
+                    tournamnet = {}
+        else:
+            self.bot.logger("There is no tournament file, creating.")
+            with open(Path(self.bot.DATA_FOLDER, f'{self.bot.name}.tournaments'), 'wb')  as fp:
+                tournament = {}
+                dump(tournament, fp)
+        return tournament
+
+    async def save_tournaments(self, tournaments):
+        print(tournaments)
+        with open(Path(self.bot.DATA_FOLDER, f'{self.bot.name}.tournaments'), 'wb')  as fp:
+            dump(tournaments, fp)
+        
+    async def add_tournament(self, url, name, chan):
+        info = {}
+        info['url'] = url
+        info['round'] = 10
+        info['players'] = set()
+        info['title'] = name
+        info['channel'] = chan
+        await self.tournament_lock.acquire()
+        tournaments = await self.load_tournaments()
+        namestring = f"name {chan.id}"
+        if namestring in tournaments:
+            self.tournament_lock.release()
+            raise ValueError("Tournament name is already taken in this channel")
+        tournaments[f"name {chan.id}"] = info
+        await self.save_tournaments(tournaments)
+        self.tournament_lock.release()
+        
+    async def add_player(self, title, name):
+        await self.tournament_lock.acquire()
+        tournaments = await self.load_tournaments()
+        if title not in tournaments:
+            self.tournament_lock.release()
+            raise ValueError("Tournament ID is not being tracked")
+        tournaments[title]['players'].add(name)
+        await self.save_tournaments(tournaments)
+        self.tournament_lock.release()
+        
+    @ChatBot.action('[Tournament Base URL] [Name]')
+    async def tournament(self, args, mobj):
+        """
+        Starts tracking a tournament. Base url is of the form
+            http://magic.wizards.com/en/events/coverage/[CHANGETHISPART]/
+        SCG still work in progress.
+        Name is one word.
+        Exampe:
+            !tournament http://magic.wizards.com/en/events/coverage/gpdc/ GPDC
+        """
+        try:
+            await self.add_tournament(args[0], args[1], mobj.channel)
+        except Exception as e:
+            return await self.message(mobj.channel, f"{e}")
+        
+    @ChatBot.action('[Tournament Name] [Player]')
+    async def track(self, args, mobj):
+        """
+        Tracks a player in the specified tournament.
+        Example:
+            !track GPDC Sukenik
+        """
+        try:
+            await self.add_player(args[0], f"{' '.join(args[1:]).strip()}")
+        except Exception as e:
+            return await self.message(mobj.channel, f"{e}")
+        return await self.message(mobj.channel, "Player is now being tracked.")
+    
+
+            
+        
+    async def check(self):
+        """
+        Sleeper thread. Checks every 5 minutes for tournament updates and then posts them to the respective channels.
+        """
+        await sleep(5)
+        while(True):
+            await self.tournament_lock.acquire()
+            tournaments = await self.load_tournaments()   
+            removals = []
+            for named_tourny in tournaments:
+                cur = tournaments[named_tourny]
+                url = self.generate_url(cur['url'], cur['round'])
+                parsed_page = self.parse_url(url, cur['players'])
+                if not parsed_page:
+                    continue
+                parsed_players = self.parse_players(parsed_page, cur['round'])
+                
+                if parsed_players == {}:
+                    continue
+                    
+                embed = Embed(
+                    title=f"{cur['title']} Round {cur['round']}",
+                    colour=Color(0x7289da),
+                )
+                
+                for playa, nfo in parsed_players.items():
+                    embed.add_field(name=playa, value=f"In {self.ordinalize(nfo['place'])} place with a record of {nfo['record']}", inline=False)
+                    
+                await self.bot.embed(cur['channel'], embed)
+                
+                cur['round'] += 1
+                
+                if cur['round'] == 16: removals.append(named_tourny)
+            
+            for deletion in removals:
+                del tournaments[deletion]
+            
+            await self.save_tournaments(tournaments)
+            self.tournament_lock.release()
+            
+            await sleep(300)
+                
+    
+        
+    def parse_url(self, url, player_list):
+        """
+        Parses the provided url to get the player objects.
+        """        
+        resp = get(url)
+        if resp.status_code != 200:
+            return None
+        bs = BS(resp.text, 'html.parser')
+        tags = bs.find_all('tr')
+        players = [x for x in tags if any([y in x.text for y in player_list])]
+        return players
+        
+    def calculate_score(self, score, round):
+        """
+        Calculates the record of the player. Assumations are that a player will not have more than 2 draws, as having 3 draws is the same as having 2 losses.
+        """        
+        score = int(score)
+        max_wins = score // 3
+        max_draws = score - (max_wins * 3)
+        max_losses = round - max_wins - max_draws
+        return f"{max_wins}-{max_losses}-{max_draws}"
+        
+    def parse_players(self, players, current_round):
+        ans_dict = {}
+        for cur in players:
+            player_dict = {}
+            fields = cur.find_all('td')
+            player_dict['place'] = fields[0].get_text()
+            player_dict['points'] = int(fields[2].get_text())
+            player_dict['record'] = self.calculate_score(player_dict['points'], current_round)
+            ans_dict[fields[1].get_text()] = player_dict
+        return ans_dict
+        
+    def generate_url(self, base, current_round):
+        if "wizards" in base:
+            d = date.today()
+            url = f"http://magic.wizards.com/en/events/coverage/gpdc/round-{current_round}-standings-{d.strftime('%Y-%m-%d')}"
+        if "starcity" in base:
+            pass #have to figure out the formatting
+        return url
+
+        
+def setup(bot):
+    TOURNAMENTBOT(bot)
